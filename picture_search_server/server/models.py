f797c58f3337c0288ec7885dd1964ea72f878446
Joseph Lefebvre <josephlefebvre1998@gmail.com>
2020-01-22 17:22:32 +0100

    set models before validation

--- picture_search_server/server/models.py
+++ picture_search_server/server/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Results(models.Model):
+    label = models.CharField(max_length=100, blank=True, default='')
+    score = models.DecimalField(max_digits=10,decimal_places=8)
+    url = models.CharField(max_length=100, blank=True, default='')
+
+
+class ImgSearchObject(models.Model):
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    date = models.DateTimeField(auto_now_add=False)
+    client = models.CharField(max_length=100, blank=True, default='')
+    results = models.ManyToManyField(Results)
+
+    class Meta:
+        ordering = ('date',)
