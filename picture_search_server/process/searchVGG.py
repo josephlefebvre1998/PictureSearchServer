e6f92b0e37d4ac9310d024c5b9ea0b041fe87807
Joseph Lefebvre <josephlefebvre1998@gmail.com>
2020-02-13 18:16:55 +0100

    add extract_cnn_vgg16_keras and use it

--- picture_search_server/process/searchVGG.py
+++ picture_search_server/process/searchVGG.py
@@ -2,37 +2,79 @@ from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 from keras.applications.imagenet_utils import decode_predictions
+from process.extract_cnn_vgg16_keras import VGGNet
 from keras.models import load_model
 from keras import models
 from keras import layers
 import numpy as np
 import os
+import h5py
 
 
 def model_load():
-    model = load_model(os.path.normpath(os.path.join(os.getcwd(), "./models/model_deep_fashion_2.h5")))
-    # model.summary()
-    print("load success")
-    return model
+    # model = load_model(os.path.normpath(os.path.join(os.getcwd(), "./models/featureDNN.h5")))
+    # # model.summary()
+    # print("load success")
+    # return model
+    h5f = h5py.File(os.path.normpath(os.path.join(os.getcwd(), "./models/featureCNN.h5")), 'r')
+    feats = h5f['dataset_feat'][:]
+    img_names = h5f['dataset_name'][:]
+    h5f.close()
+    return feats, img_names
 
 
 def predict(img):
-    numpy_image = image.img_to_array(img)
-    image_batch = np.expand_dims(numpy_image, axis=0)
-    processed_image = preprocess_input(image_batch.copy())
+    # numpy_image = image.img_to_array(img)
+    # image_batch = np.expand_dims(numpy_image, axis=0)
+    # processed_image = preprocess_input(image_batch.copy())
+    #
+    # predictions = model.predict(processed_image)
+    # result = decode_predictions(predictions)
+    # init VGGNet16 model
+    model = VGGNet()
 
-    predictions = model.predict(processed_image)
-    result = decode_predictions(predictions)
-    return result
+    # extract query image's feature, compute simlarity score and sort
+    queryVec = model.extract_feat_image(img)
+    scores = np.dot(queryVec, feats.T)
+    rank_ID = np.argsort(scores)[::-1]
+    rank_score = scores[rank_ID]
+    # print rank_ID
+    # print rank_score
+
+    # number of top retrieved images to show
+    maxres = 3
+    imlist = [images[index] for i, index in enumerate(rank_ID[0:maxres])]
+    return imlist
 
 
 def predict_test():
+    # img_path = os.path.normpath(os.path.join(os.getcwd(), "./tests_images/20180809_130409.jpg"))
+    # image_load = image.load_img(img_path, target_size=(150, 150))
+    # print(predict(image_load))
+
+    # # CNN retrieval
     img_path = os.path.normpath(os.path.join(os.getcwd(), "./tests_images/20180809_130409.jpg"))
-    image_load = image.load_img(img_path, target_size=(150, 150))
-    print(predict(image_load))
+
+    # init VGGNet16 model
+    model = VGGNet()
+
+    # extract query image's feature, compute simlarity score and sort
+    queryVec = model.extract_feat_path(img_path)
+    scores = np.dot(queryVec, feats.T)
+    rank_ID = np.argsort(scores)[::-1]
+    rank_score = scores[rank_ID]
+    # print rank_ID
+    # print rank_score
+
+    # number of top retrieved images to show
+    maxres = 3
+    imlist = [images[index] for i, index in enumerate(rank_ID[0:maxres])]
+    # print ("top %d images in order are: " (str(maxres), imlist))
+    print(imlist)
+    return imlist
 
 
-model = model_load()
+feats, images = model_load()
 
 if __name__ == '__main__':
     predict_test()
